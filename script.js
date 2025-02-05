f4c2b3375ddb81c8cb6dae6ff16cc54550bac9da
Daniel Tran <68958503+Uteki@users.noreply.github.com>
2025-02-05 23:55:38 +0100

    updates for the project and order structure changes

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log(books);
+
+// TODO - create template
+
+// TODO - iterate through books and put into template multiply times
+
+// TODO - more?
+
+
+
+// TODO - ... create liking/disliking option
+
+// TODO - ... add comments etc.
+
+// TODO - ... local storage saving?
